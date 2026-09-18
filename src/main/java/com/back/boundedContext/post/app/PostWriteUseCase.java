5feb913371760082cc058bc681d0a70fa39f51b4
J <air92323@gamil.com>
2026-09-18 09:49:26 +0900

    0011

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -3,6 +3,10 @@ package com.back.boundedContext.post.app;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
+import com.back.global.eventPublisher.EventPublisher;
+import com.back.global.rsData.RsData;
+import com.back.shared.post.dto.PostDto;
+import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +16,21 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PostWriteUseCase {
     private final PostRepository postRepository;
+    private final EventPublisher eventPublisher;
 
     public long count() {
         return postRepository.count();
     }
 
-    public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         Post post = new Post(author, title, content);
 
-        //TODO: 이벤트 수정
-        author.increaseActivityScore(3);
+        eventPublisher.publish(
+                new PostCreatedEvent(
+                        new PostDto(post)
+                )
+        );
 
-        return postRepository.save(post);
-    }
-
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
     }
 }
